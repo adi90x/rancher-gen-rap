e285f0bd10522822b1098dfde78175988e6a7a5f
AdrienM <amaurel90@gmail.com>
2016-12-09 14:44:30 +0000

    Update template_functions.go

--- template_functions.go
+++ template_functions.go
@@ -25,13 +25,13 @@ func exists(path string) (bool, error) {
 
 func dict(values ...interface{}) (map[string]interface{}, error) {
 	if len(values)%2 != 0 {
-		return nil, errors.New("invalid dict call")
+		return nil, fmt.Errorf("invalid dict call")
 	}
 	dict := make(map[string]interface{}, len(values)/2)
 	for i := 0; i < len(values); i += 2 {
 		key, ok := values[i].(string)
 		if !ok {
-			return nil, errors.New("dict keys must be strings")
+			return nil, fmt.Errorf("dict keys must be strings")
 		}
 		dict[key] = values[i+1]
 	}
