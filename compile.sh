32d75735458755e06688901c7e4f9069a3302742
AdrienM <amaurel90@gmail.com>
2017-01-15 13:59:06 +0100

    Add dependencies needed in script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 export GOPATH=$(pwd)/Godeps
+go get github.com/BurntSushi/toml
+go get github.com/Sirupsen/logrus
+go get github.com/rancher/go-rancher-metadata/metadata
 go build -tags netgo -a -v  -ldflags "-X main.GitSHA=LocalBuild"
